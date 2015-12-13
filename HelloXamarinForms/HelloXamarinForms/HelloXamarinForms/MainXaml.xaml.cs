519cf2c1dddeb2ed236530f12039a7e599c4fbca
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2015-12-13 17:43:28 +0900

    First Upload

---
+++ HelloXamarinForms/HelloXamarinForms/HelloXamarinForms/MainXaml.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HelloXamarinForms
+{
+    public partial class MainXaml : ContentPage
+    {
+        public MainXaml()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked (object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SubCS(entry.Text));
+        }
+    }
+}
---
+++ HelloXamarinForms/HelloXamarinForms/HelloXamarinForms/SubCS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace HelloXamarinForms
+{
+    public class SubCS : ContentPage
+    {
+        public SubCS(string name)
+        {
+            Title = "Welcome";
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    new Label {
+                        Text = string.Format("Hello {0}!", name),
+                        HorizontalTextAlignment = TextAlignment.Center
+                    }
+                }
+            };
+        }
+    }
+}
